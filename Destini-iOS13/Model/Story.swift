978c895a08c4f093e291042bb606b5d160d75626
Cem Safa <cemsafa@gmail.com>
2020-07-12 14:11:01 +0300

    initial commit

---
+++ Destini-iOS13/Model/Story.swift
@@ -0,0 +1,17 @@
+//
+//  Sotry.swift
+//  Destini-iOS13
+//
+//  Created by Cem Safa on 9.07.2020.
+//  Copyright © 2020 Cem Safa. All rights reserved.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
